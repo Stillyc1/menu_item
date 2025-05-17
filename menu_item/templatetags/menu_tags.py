380e29b7932cffb1afc187875bb45c86f10dfed4
Alexander_Baranov <Sti_ll@mail.ru>
2025-05-17 10:40:11 +0300

    add menu_tags.py, generate_html_menu

--- menu_item/templatetags/menu_tags.py
+++ menu_item/templatetags/menu_tags.py
@@ -1,41 +1,61 @@
 from django import template
+from django.db import connection
 from django.utils.html import format_html
-
-from menu_item.models import MenuItem
 
 register = template.Library()
 
 
 @register.simple_tag
-def draw_menu(title_menu):
-    menu_obj = MenuItem.objects.get(title=title_menu)
-    return generate_menu(menu_obj)
+def draw_menu(title_menu: str):
+    """ Прямой запрос в БД получаем объект и весь корневой каталог меню до главного."""
+    with connection.cursor() as cursor:
+        query = """
+        WITH RECURSIVE parent_hierarchy AS (
+            SELECT id, title, parent_id
+            FROM menu_item_menuitem
+            WHERE title = %s
+
+            UNION ALL
+
+            SELECT m.id, m.title, m.parent_id
+            FROM menu_item_menuitem m
+            JOIN parent_hierarchy ph ON m.id = ph.parent_id
+        ),
+        submenus AS (
+            SELECT id, title, parent_id
+            FROM menu_item_menuitem
+            WHERE parent_id = (SELECT id FROM menu_item_menuitem WHERE title = %s)
+        )
+        SELECT * FROM parent_hierarchy
+        UNION ALL
+        SELECT * FROM submenus;
+        """
+        cursor.execute(query, [title_menu, title_menu])
+        results = cursor.fetchall()
+        results = {row[0]: {'id': row[0], 'title': row[1], 'parent_id': row[2]} for row in results}
+
+        return generate_menu(results, title_menu)
 
 
-def generate_menu(menu_obj):
-    parent = menu_obj.parent
-    if not parent:
-        ul = '<ul>'
-        ul += f'<li>{menu_obj.title}'
+def generate_menu(items: dict, title_menu: str):
+    if not items:
+        return None
+
+    hierarchy_menu = sorted(items.items(), key=lambda item: item)  # Сортируем объекты от корневого меню
+    hierarchy_menu_iter = iter(hierarchy_menu)  # Итератор для удобства вывода объектов в html
+
+    ul = '<ul>'
+    for i in range(len(hierarchy_menu)):
+        title = next(hierarchy_menu_iter)[1]['title']  # Достаем следующий объект из итератора
+        if title == title_menu:
+            ul += f'<li><a href="/{title}/">{title}</a><ul>'
+            try:
+                while True:
+                    title = next(hierarchy_menu_iter)[1]['title']  # Выводим первый уровень вложенности подменю.
+                    ul += f'<li><a href="/{title}/">{title}</a></li>'
+            except StopIteration:
+                break
+        ul += f'<li><a href="/{title}/">{title}</a>'
         ul += '<ul>'
-        print(ul)
-        for submenu in menu_obj.submenu.all():
-            ul += f'<li>{submenu.title}</li>'
-        ul += '</ul>'
-        ul += '</li>'
-        form_html = format_html(ul)
-        print(form_html)
-        return form_html
-    generate_menu(menu_obj.parent)
 
-    # if menu_obj.parent:
-    #     ul += f'<li>{menu_obj.parent.title}<ul>'
-    #
-    # ul += f'<li>{menu_obj.title}'
-    #
-    # if menu_obj.submenu.all():
-    #     ul += '<ul>'
-    #     for submenu in menu_obj.submenu.all():
-    #         ul += f'<li>{submenu.title}</li>'
-    #     ul += '</ul>'
-    # ul += '</ul>'
+    return format_html(ul)
