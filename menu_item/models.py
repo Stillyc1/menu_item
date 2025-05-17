7174e315515899db036f8a0abe2004342e97efd9
Alexander_Baranov <Sti_ll@mail.ru>
2025-05-17 10:38:52 +0300

    change_model

--- menu_item/models.py
+++ menu_item/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class MenuItem(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='submenu')
 
     def __str__(self):
