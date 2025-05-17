d326c992d00257ce6429d45964f7ad1c2302010e
Alexander_Baranov <Sti_ll@mail.ru>
2025-05-17 10:39:20 +0300

    add BaseClassCOntext

--- menu_item/views.py
+++ menu_item/views.py
@@ -3,7 +3,15 @@ from django.views.generic import ListView
 from menu_item.models import MenuItem
 
 
-class MenuView(ListView):
+class BaseContextView(ListView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = self.kwargs.get('title')
+        return context
+
+
+class MenuView(BaseContextView):
     """Класс представления меню в шаблоне."""
 
     model = MenuItem
